085ad4010365173ea0a06ac3be80223df08600b8
QuentinVigier <vigierquentinpro@gmail.com>
2024-10-16 16:35:16 +0200

    Mise en place des filtres de recherche avancée dans la page Rooms

--- src/Controller/RoomController.php
+++ src/Controller/RoomController.php
@@ -3,19 +3,35 @@
 namespace App\Controller;
 
 use App\Entity\Room;
+use App\Repository\EquipmentRepository;
+use App\Repository\ErgonomicRepository;
 use App\Repository\RoomRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class RoomController extends AbstractController
 {
     #[Route('/rooms', name: 'app_rooms_all', methods: 'GET')]
-    public function all(RoomRepository $roomRepository): Response
+    public function all(RoomRepository $roomRepository, 
+        EquipmentRepository $equipmentRepository,
+        ErgonomicRepository $ergonomicRepository,
+        Request $request): Response
     {
-        $rooms = $roomRepository->findAll();
+        $capacity = $request->query->get('capacity');
+        $equipmentIds = $request->query->all('equipment');
+        $ergonomicIds = $request->query->all('ergonomics');
+        
+        $rooms = $roomRepository->findBySearchCriteria($capacity, $equipmentIds, $ergonomicIds);
+        $equipments = $equipmentRepository->findAll();
+        $ergonomics = $ergonomicRepository->findAll();
+
+
         return $this->render('room/all.html.twig', [
             'rooms' => $rooms,
+            'equipments' => $equipments,
+            'ergonomics' => $ergonomics,
         ]);
     }
 
--- src/Repository/RoomRepository.php
+++ src/Repository/RoomRepository.php
@@ -16,28 +16,54 @@ class RoomRepository extends ServiceEntityRepository
         parent::__construct($registry, Room::class);
     }
 
-//    /**
-//     * @return Room[] Returns an array of Room objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('r')
-//            ->andWhere('r.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('r.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+    public function findBySearchCriteria(?int $capacity, array $equipmentIds, array $ergonomicIds)
+    {
+        $qb = $this->createQueryBuilder('r');
+    
+        if ($capacity !== null) {
+            $qb->andWhere('r.capacity >= :capacity')
+                ->setParameter('capacity', $capacity);
+        }
+    
+        if (!empty($equipmentIds)) {
+            $qb->leftJoin('r.roomEquipment', 're')
+                ->leftJoin('re.equipment', 'e')
+                ->andWhere('e.id IN (:equipmentIds)')
+                ->setParameter('equipmentIds', $equipmentIds);
+        }
+    
+        if (!empty($ergonomicIds)) {
+            $qb->leftJoin('r.room_ergonomics', 'rerg')
+                ->leftJoin('rerg.ergonomic', 'erg')
+                ->andWhere('erg.id IN (:ergonomicIds)')
+                ->setParameter('ergonomicIds', $ergonomicIds);
+        }
+    
+        return $qb->getQuery()->getResult();
+    }
 
-//    public function findOneBySomeField($value): ?Room
-//    {
-//        return $this->createQueryBuilder('r')
-//            ->andWhere('r.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+    //    /**
+    //     * @return Room[] Returns an array of Room objects
+    //     */
+    //    public function findByExampleField($value): array
+    //    {
+    //        return $this->createQueryBuilder('r')
+    //            ->andWhere('r.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->orderBy('r.id', 'ASC')
+    //            ->setMaxResults(10)
+    //            ->getQuery()
+    //            ->getResult()
+    //        ;
+    //    }
+
+    //    public function findOneBySomeField($value): ?Room
+    //    {
+    //        return $this->createQueryBuilder('r')
+    //            ->andWhere('r.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->getQuery()
+    //            ->getOneOrNullResult()
+    //        ;
+    //    }
 }
