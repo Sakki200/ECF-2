4505ee7da839d6adc5c315ae68487bc201276617
QuentinVigier <vigierquentinpro@gmail.com>
2024-10-16 14:54:00 +0200

    Récupération des données dans les pages rooms + Création de la NavBar + Création de la grid pour afficher les rooms

--- src/Controller/RoomController.php
+++ src/Controller/RoomController.php
@@ -2,17 +2,28 @@
 
 namespace App\Controller;
 
+use App\Entity\Room;
+use App\Repository\RoomRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class RoomController extends AbstractController
 {
-    #[Route('/room', name: 'app_room')]
-    public function index(): Response
+    #[Route('/rooms', name: 'app_rooms_all', methods: 'GET')]
+    public function all(RoomRepository $roomRepository): Response
     {
-        return $this->render('room/index.html.twig', [
-            'controller_name' => 'RoomController',
+        $rooms = $roomRepository->findAll();
+        return $this->render('room/all.html.twig', [
+            'rooms' => $rooms,
+        ]);
+    }
+
+    #[Route('/rooms/{id}', name: 'app_room_show', methods: 'GET')]
+    public function show(Room $room): Response
+    {
+        return $this->render('room/show.html.twig', [
+            'room' => $room,
         ]);
     }
 }
